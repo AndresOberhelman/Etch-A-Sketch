7016e587c01c7b07ffdc76c23fcbcfffa615976b
AndresOberhelman <andres.w.oberhelman@gmail.com>
2022-07-06 00:58:58 -0500

    Added squares which are blue for now

--- app.js
+++
@@ -1,7 +0,0 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns= "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16 , 1fr)";
-
-for(let i=0; 1<10; i++)
-{
-}
